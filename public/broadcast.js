19981e6aebe0f302e7d1ffeb89c92c03ee8f42ec
TannerGabriel <gabrieltanner.code@gmail.com>
2020-01-28 15:58:07 +0100

    Choose camera and audio options

--- public/broadcast.js
+++ public/broadcast.js
@@ -8,22 +8,6 @@ const config = {
 };
 
 const socket = io.connect(window.location.origin);
-const video = document.querySelector("video");
-
-// Media contrains
-const constraints = {
-  video: { facingMode: "user" }
-  // Uncomment to enable audio
-  // audio: true,
-};
-
-navigator.mediaDevices
-  .getUserMedia(constraints)
-  .then(stream => {
-    video.srcObject = stream;
-    socket.emit("broadcaster");
-  })
-  .catch(error => console.error(error));
 
 socket.on("answer", (id, description) => {
   peerConnections[id].setRemoteDescription(description);
@@ -33,8 +17,14 @@ socket.on("watcher", id => {
   const peerConnection = new RTCPeerConnection(config);
   peerConnections[id] = peerConnection;
 
-  let stream = video.srcObject;
+  let stream = videoElement.srcObject;
   stream.getTracks().forEach(track => peerConnection.addTrack(track, stream));
+
+  peerConnection.onicecandidate = event => {
+    if (event.candidate) {
+      socket.emit("candidate", id, event.candidate);
+    }
+  };
 
   peerConnection
     .createOffer()
@@ -42,12 +32,6 @@ socket.on("watcher", id => {
     .then(() => {
       socket.emit("offer", id, peerConnection.localDescription);
     });
-
-  peerConnection.onicecandidate = event => {
-    if (event.candidate) {
-      socket.emit("candidate", id, event.candidate);
-    }
-  };
 });
 
 socket.on("candidate", (id, candidate) => {
@@ -62,3 +46,68 @@ socket.on("disconnectPeer", id => {
 window.onunload = window.onbeforeunload = () => {
   socket.close();
 };
+
+// Get camera and microphone
+const videoElement = document.querySelector("video");
+const audioSelect = document.querySelector("select#audioSource");
+const videoSelect = document.querySelector("select#videoSource");
+
+audioSelect.onchange = getStream;
+videoSelect.onchange = getStream;
+
+getStream()
+  .then(getDevices)
+  .then(gotDevices);
+
+function getDevices() {
+  return navigator.mediaDevices.enumerateDevices();
+}
+
+function gotDevices(deviceInfos) {
+  window.deviceInfos = deviceInfos;
+  for (const deviceInfo of deviceInfos) {
+    const option = document.createElement("option");
+    option.value = deviceInfo.deviceId;
+    if (deviceInfo.kind === "audioinput") {
+      option.text = deviceInfo.label || `Microphone ${audioSelect.length + 1}`;
+      audioSelect.appendChild(option);
+    } else if (deviceInfo.kind === "videoinput") {
+      option.text = deviceInfo.label || `Camera ${videoSelect.length + 1}`;
+      videoSelect.appendChild(option);
+    }
+  }
+}
+
+function getStream() {
+  if (window.stream) {
+    window.stream.getTracks().forEach(track => {
+      track.stop();
+    });
+  }
+  const audioSource = audioSelect.value;
+  const videoSource = videoSelect.value;
+  const constraints = {
+    audio: { deviceId: audioSource ? { exact: audioSource } : undefined },
+    video: { deviceId: videoSource ? { exact: videoSource } : undefined }
+  };
+  return navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then(gotStream)
+    .catch(handleError);
+}
+
+function gotStream(stream) {
+  window.stream = stream;
+  audioSelect.selectedIndex = [...audioSelect.options].findIndex(
+    option => option.text === stream.getAudioTracks()[0].label
+  );
+  videoSelect.selectedIndex = [...videoSelect.options].findIndex(
+    option => option.text === stream.getVideoTracks()[0].label
+  );
+  videoElement.srcObject = stream;
+  socket.emit("broadcaster");
+}
+
+function handleError(error) {
+  console.error("Error: ", error);
+}
